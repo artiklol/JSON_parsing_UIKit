eebcf094032ab747c5550d4e7722892e058ef2a0
Artsiom <artiklol2012@gmail.com>
2022-11-05 23:16:35 +0300

    Initial Commit

---
+++ JSON_parsing_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON_parsing_UIKit
+//
+//  Created by Artem Sulzhenko on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
